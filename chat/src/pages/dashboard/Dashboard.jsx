ff9987d64ddd5660a2f09b3708defdb6061e55e3
Migueleangel <miguel.sotelo@uao.edu.co>
2024-03-08 13:55:30 -0500

    visual dashboard

--- chat/src/pages/dashboard/Dashboard.jsx
+++ chat/src/pages/dashboard/Dashboard.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { useAuth } from '../../context/AuthContext'
-const Dashboard = () => {
+import React from 'react';
+import { useAuth } from '../../context/AuthContext';
+import './Dashboard.css'; // Import the CSS file
+import AnimatedD from './animationD/animatedD';
 
-  const {user} = useAuth()
+const Dashboard = () => {
+  const { user } = useAuth();
 
   return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}> 
-      <h1>Bienvenido {user?.usuario}</h1>
+    <div className="wrapper">
+      <div className="wrapper-left">
+        <div className="content">
+          <h1>Bienvenido {user?.usuario}</h1>
+        </div>
+      </div>
+      <div className="wrapper-basic wrapper-right">
+        <AnimatedD />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
